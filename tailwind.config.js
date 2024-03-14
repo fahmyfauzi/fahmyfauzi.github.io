4142774b14bf0f4034f5f9615819346075137050
Fahmy <fahmyfauzii@gmail.com>
2024-03-14 08:56:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4891e0",
+        Secondary: "#f9c24c",
+        dark: "#31313a",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
